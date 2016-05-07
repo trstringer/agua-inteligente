a66d867b7e58ea3e106bb12fcb9075e3f3c54bd0
Thomas Stringer <tr.stringer@outlook.com>
2016-05-07 10:40:45 -0400

    WIP: Creating controller service

---
+++ mobile-app/AguaInteligente/AguaInteligente/ControllerService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AguaInteligente
+{
+    public class ControllerService
+    {
+        public Uri ServiceUri { get; }
+
+        public ControllerService(string serviceUri)
+        {
+            ServiceUri = new Uri(serviceUri.TrimEnd('/'));
+        }
+
+        /// <summary>
+        /// connect to the service and get the status to see if 
+        /// the irrigation system is currently on (true) or 
+        /// off (false)
+        /// </summary>
+        public GetIrrigationStatus()
+        {
+
+        }
+    }
+}
+
